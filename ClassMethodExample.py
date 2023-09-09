1438c8f16cd1921f82cb3dd401e2bd3bd752280b
chanhoGod <cksgh3422@gmail.com>
2023-09-09 12:07:47 +0900

    0909 Python Study

--- ClassMethodExample.py
+++ ClassMethodExample.py
@@ -3,7 +3,7 @@ class Car:
     def __init__(self, body_type):
         self.body_type = body_type
 
-    def __repr__(self):
+    def __repr__(self):         ## 클래스를 프린트했을때 나오는 메서드
         return f'Car({self.body_type} body type)'
 
     def set_body_type(self, body_type):
@@ -21,8 +21,9 @@ class Car:
 # c = Car(body_type="sport")
 # c.set_body_type("sedan")
 # print(c.body_type)
-# print(Car.hyundai())
-print(Car.ferrari())
+# print(c.body_type)
+print(Car.hyundai())
+# print(Car.ferrari())
 
 # class methods take a cls parameter that points to the class—and not the object instance—when the method is called
 # Because the class method only has access to this cls argument, it can’t modify object instance state. That would require access to self. However, class methods can still modify class state that applies across all instances of the class.
---
+++ NameSpecialMethodExample.py
@@ -0,0 +1,11 @@
+# __name__
+
+# __name__ = "__main__":
+
+def my_func1():
+    print("myfunc1")
+
+if __name__ == "__main__":
+    print("one.py has run directly")
+else:
+    print("one.py has imported")
